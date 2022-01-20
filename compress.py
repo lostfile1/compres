23ecee09a414cb1d9956bc4eb535935b7c9fabd9
WhitePaperKat <41842361+lostfile1@users.noreply.github.com>
2022-01-20 17:21:59 -0500

    Update compress.py
    working on switches

--- compress.py
+++ compress.py
@@ -3,12 +3,15 @@ import os
 import base64
 import zlib
 
-with open(sys.argv[1],'r') as file1:
+with open(sys.argv[2],'r') as file1:
  data = file1.read()
 
-if sys.argv[2] =="c":
+if sys.argv[1] =="c":
     code = base64.b64encode(zlib.compress(data.encode('utf-8'),8))
     code = code.decode('utf-8')
     f = open('pressed.txt','w')
     f.write(code)
     f.close()
+    
+if 
+    
